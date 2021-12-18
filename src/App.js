7dbc6a1e1f8db7f30e99733c77a2585c39b42a45
Jason Kim <jason.s.kim95@gmail.com>
2021-12-18 13:44:56 -0500

    Create app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import "./App.css";
+import { Gameboard } from "./components/Gameboard";
+import { Scoreboard } from "./components/Scoreboard";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const addPoint = () => {
+    setCurrentScore(currentScore + 1);
+  };
+
+  const evaluateBestScore = () => {
+    if (currentScore > bestScore) {
+      setBestScore(currentScore);
+    }
+  };
+
+  const resetScore = () => {
+    setCurrentScore(0);
+  };
+
+  return (
+    <div className="App">
+      <Scoreboard currentScore={currentScore} bestScore={bestScore} />
+      <Gameboard
+        addPoint={addPoint}
+        evaluateBestScore={evaluateBestScore}
+        resetScore={resetScore}
+      />
+    </div>
+  );
 }
 
 export default App;
